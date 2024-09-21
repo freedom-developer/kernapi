4ba46fa2873fe52953d0e7722398546553ca65b4
freedom-developer <freedom_developer@163.com>
2024-09-21 18:06:14 +0800

    add cdev

---
+++ cdev/cdev.c
@@ -0,0 +1,51 @@
+#include <linux/module.h>
+#include <linux/init.h>
+
+#include <linux/device.h>
+
+MODULE_LICENSE("GPL");
+
+struct bus_type my_bus = {
+    .name = "wsb",
+};
+
+struct device my_dev = {
+    .init_name = "wsb_dev",
+};
+
+static int __init cdev_init(void)
+{
+    int ret;
+
+    ret = bus_register(&my_bus);
+    if (ret < 0) {
+        printk(KERN_ERR "register my bus failed\n");
+        return -1;
+    }
+
+    ret = device_register(&my_dev);
+    if (ret < 0) {
+        bus_unregister(&my_bus);
+        printk(KERN_ERR "add my_dev failed\n");
+
+        return -1;
+    }
+    
+    printk(KERN_INFO "cdev module initialize OK!\n");
+    return 0;
+}
+
+static void __exit cdev_exit(void)
+{
+    device_unregister(&my_dev);
+    bus_unregister(&my_bus);
+
+    printk(KERN_WARNING"cdev module exit ok!\n");
+}
+
+
+module_init(cdev_init);
+module_exit(cdev_exit);
+
+
+
