9f44be94338b739a86b97de4df31ee0db15820be
freedom-developer <freedom_developer@163.com>
2024-09-22 17:12:19 +0800

    update

--- cdev/cdev.c
+++ cdev/cdev.c
@@ -9,8 +9,21 @@ struct bus_type my_bus = {
     .name = "wsb",
 };
 
+static void mydev_release(struct device *dev)
+{
+    printk(KERN_INFO "release my_dev device\n");
+}
+
 struct device my_dev = {
     .init_name = "wsb_dev",
+    .bus = &my_bus,
+    .release = mydev_release,
+};
+
+struct device_driver my_driver = {
+    .name = "wsb_driver",
+    .bus = &my_bus,
+    .owner = THIS_MODULE
 };
 
 static int __init cdev_init(void)
@@ -30,6 +43,16 @@ static int __init cdev_init(void)
 
         return -1;
     }
+
+    ret = driver_register(&my_driver);
+    if (ret < 0) {
+        printk(KERN_ERR "register my_driver failed\n");
+        device_unregister(&my_dev);
+        bus_unregister(&my_bus);
+        return -1;
+    }
+
+
     
     printk(KERN_INFO "cdev module initialize OK!\n");
     return 0;
@@ -39,6 +62,7 @@ static void __exit cdev_exit(void)
 {
     device_unregister(&my_dev);
     bus_unregister(&my_bus);
+    driver_unregister(&my_driver);
 
     printk(KERN_WARNING"cdev module exit ok!\n");
 }
